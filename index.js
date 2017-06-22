fa40ed899762731d6c08d46cc60560ce2e51fd70
fangqing <fangqing@114study.com>
2017-06-22 17:30:34 +0800

    'update'

--- index.js
+++ index.js
@@ -2,35 +2,37 @@
     常用JS操作库包括
     create by brandon fang 2017-05-26
 */
-var base = require('./modules/base');
-var https = require('./modules/https');
-var dom = require('./modules/dom');
-var msg = require('./modules/msg');
-var tool = require('./modules/tool');
-var array = require('./modules/array');
-var string = require('./modules/string');
-var object = require('./modules/object');
-var event = require('./modules/event');
-var uri = require('./modules/uri');
-var _window = require('./modules/window');
-var cookie = require('./modules/res/cookie');
+import base from './modules/base';
+import https from './modules/https';
+import dom from './modules/dom';
+import msg from './modules/msg';
+import tool from './modules/tool';
+import _array from './modules/array'
+import _string from './modules/string';
+import _object from './modules/object';
+import _event from './modules/event';
+import _uri from './modules/uri';
+import _window from './modules/window';
+import _cookie from './modules/res/cookie';
 
-base.prototype = {
+let container = function(){};
+
+container.prototype = {
 	constructor : base,
 	https : https,
 	dom : dom,
 	msg : msg,
-	array : array,
-	string : string,
+	array : _array,
+	string : _string,
 	tool : tool,
-	object : object,
-	event : event,
-	uri : uri,
+	object : _object,
+	event : _event,
+	uri : _uri,
 	window : _window,
-	cookie : cookie,
+	cookie : _cookie,
 	log : base.prototype.log,
 }
 
 ~function(W){
-	W.bt = new base();;
+	W.bt = new container();
 }(window);
